d5a1ccdf18fa50bd4d85b65c9899eeb984709228
DanielScheffel <109425652+DanielScheffel@users.noreply.github.com>
2022-10-02 18:26:09 -0300

    Add files via upload

---
+++ atividade d temperatura/js/script.js
@@ -0,0 +1,51 @@
+document.querySelector("#button-celsius").onclick = function(){
+    let celsius = Number (document.querySelector("#celsius").value);
+    let fahrenheitInput = document.querySelector("#fahrenheit");
+    let kelvinInput = document.querySelector("#kelvin");
+    let rankineInput = document.querySelector("#rankine");
+
+    fahrenheitInput.setAttribute("placeholder", +(celsius*(9/5)+32).toFixed(3));
+    kelvinInput.setAttribute("placeholder", +(celsius + 273.15).toFixed(3));
+    rankineInput.setAttribute("placeholder", +(celsius * (9/5) + 491.67).toFixed(3));
+
+    return false;
+}
+document.querySelector("#button-fahrenheit").onclick = function(){
+    let fahrenheit = Number (document.querySelector("#fahrenheit").value);
+    let celsiusInput = document.querySelector("#celsius");
+    let kelvinInput = document.querySelector("#kelvin");
+    let rankineInput = document.querySelector("#rankine");
+
+    celsiusInput.setAttribute("placeholder", + (fahrenheit - 32)*(9/5).toFixed(3));
+    kelvinInput.setAttribute("placeholder", + ((fahrenheit - 32)*(9/5) + 273.15).toFixed(3));
+    rankineInput.setAttribute("placeholder", + (fahrenheit + 459.67).toFixed(3));
+
+    return false;
+}
+document.querySelector("#button-kelvin").onclick = function(){
+    let kelvin = Number (document.querySelector("#kelvin").value);
+    let celsiusInput = document.querySelector("#celsius");
+    let fahrenheitInput = document.querySelector("#fahrenheit");
+    let rankineInput = document.querySelector("#rankine");
+
+    celsiusInput.setAttribute("placeholder", + (kelvin - 273.15).toFixed(3));
+    fahrenheitInput.setAttribute("placeholder", + ((kelvin - 273.15)*(9/5) + 32).toFixed(3));
+    rankineInput.setAttribute("placeholder", + ((kelvin - 32)*(5/9) + 273.15).toFixed(3));
+
+    return false;
+}
+document.querySelector("#button-rankine").onclick = function(){
+    let rankine = Number (document.querySelector("#rankine").value);
+    let celsiusInput = document.querySelector("#celsius");
+    let fahrenheitInput = document.querySelector("#fahrenheit");
+    let kelvinInput = document.querySelector("#kelvin");
+
+    celsiusInput.setAttribute("placeholder", + (rankine - 491.67)*(5/9).toFixed(3));
+    fahrenheitInput.setAttribute("placeholder", + (rankine - 459.67).toFixed(3));
+    kelvinInput.setAttribute("placeholder", + (rankine * (5/9)).toFixed(3));
+
+    return false;
+}
+document.querySelector("#limpar").onclick = function(){
+    location.reload();
+}
